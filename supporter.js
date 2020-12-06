f2353c91496825c446938420b3cf5f615576227d
slime_turtle <61219276+turtle85917@users.noreply.github.com>
2020-12-06 11:39:16 +0900

    Delete supporter.js

--- supporter.js
+++
@@ -1,8 +0,0 @@
-var support = {};
-
-support.random = (min, max, isInt) => {
-  var ran = Math.random() * (max-min+1);
-  return isInt ? Math.floor(ran) + min : ran + min;
-}
-
-module.exports = support;
